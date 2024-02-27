8767fff8652e483c1ab0c4b60ba4ba0eba6d384a
naminov <naminov@tko-inform.ru>
2024-02-27 21:03:13 +0300

    refactor content folder and change character name

--- Source/UE4_RPG/UE4_RPGGameMode.cpp
+++ Source/UE4_RPG/UE4_RPGGameMode.cpp
@@ -1,13 +1,13 @@
 // Copyright Epic Games, Inc. All Rights Reserved.
 
 #include "UE4_RPGGameMode.h"
-#include "UE4_RPGCharacter.h"
+// #include "UE4_RPGCharacter.h"
 #include "UObject/ConstructorHelpers.h"
 
 AUE4_RPGGameMode::AUE4_RPGGameMode()
 {
 	// set default pawn class to our Blueprinted character
-	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/ThirdPersonCPP/Blueprints/ThirdPersonCharacter"));
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/Character/Behaviour/BP_Character"));
 	if (PlayerPawnBPClass.Class != NULL)
 	{
 		DefaultPawnClass = PlayerPawnBPClass.Class;
